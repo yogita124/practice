5dbd22c6b03b72701843f46c90b288f360ac021e
yogita124 <50488259+yogita124@users.noreply.github.com>
2020-02-07 13:10:43 +0530

    Update index.php

--- q-1/index.php
+++ q-1/index.php
@@ -1,12 +1,12 @@
 <?php
-    //array of student details
-    $details =  array( 
-                array('sid' => '101', 'name' => 'Yogita', 'dob' => strtotime('02-01-1999'), 'class' =>'12', 'passed' => '0','marks'=>array('P'=>'56','C  '=>'12','M'=>'50') ),
-                array('sid' => '102', 'name' => 'Zubin', 'dob' => strtotime('05-03-1998'), 'class' =>'10', 'passed' => '0','marks'=>array('SS'=>'30','S'=>'14','E'=>'03')),
-                array('sid' => '103', 'name' => 'Yash', 'dob' => strtotime('27-11-1998'), 'class' =>'11', 'passed' => '0','marks'=>array('E'=>'30','B'=>'29','M'=>'35'))
+    //hardcoded arrays of array with student details
+    $student_details =  array( 
+                array('sid' => '101', 'name' => 'Yogita', 'dob' => strtotime('19-12-1998'), 'class' =>'12', 'passed' => '0','marks'=>array('P'=>'56','C  '=>'12','M'=>'50') ),
+                array('sid' => '102', 'name' => 'Zubin', 'dob' => strtotime('02-08-1999'), 'class' =>'10', 'passed' => '0','marks'=>array('SS'=>'30','S'=>'14','E'=>'03')),
+                array('sid' => '103', 'name' => 'Yash', 'dob' => strtotime('24-03-2000'), 'class' =>'11', 'passed' => '0','marks'=>array('E'=>'30','B'=>'29','M'=>'35'))
                 );
-    //array of subjects
-    $subject =  array(
+    //array of subjects according to classes
+    $subject_details =  array(
                 12 => array(array('name' => 'Physics', 'code' => P, 'mm' => 25),
                 array('name' => 'Chemistry', 'code' => C, 'mm' => 30),
                 array('name' => 'Mathematics', 'code' => M, 'mm' => 33)),
@@ -17,7 +17,7 @@
                 array('name' => 'Sanskrit', 'code' => S, 'mm' => 40),
                 array('name' => 'English', 'code' => E, 'mm' => 40))
                  );
-    //array of obtained marks
+    //array containing marks obtained by students
     $marks = array(
             array('id' => '101', 'code' => 'P', 'ob' => '56'),
             array('id' => '101', 'code' => 'C', 'ob' => '12'),
@@ -29,28 +29,31 @@
             array('id' => '103', 'code' => 'B', 'ob' => '29'),
             array('id' => '103', 'code' => 'M', 'ob' => '35'),
             );
-    //ANSWER --1 function for subject as per class
-    function subj ($class, $subject) {         
-        $show = $subject[$class];
+    //function for subject as per class
+    function get_subject ($class, $subject_details) {         
+        $show = $subject_details[$class];
         foreach ($show as $key => $value) 
         {
             echo "<tr><td>".$value['name']."</td><td>".$value['code']."</td><td>".$value['mm']."</td></tr>";
         }
     }
     echo "<br><table border=1><tr><th>SUBJECT NAME</th><th>SUBJECT CODE</th><th>MIN. MARKS</th></tr>";
-    subj(12,$subject);
-    subj(11,$subject);
-    subj(10,$subject);
+    //function call to get subject for grade 12
+    get_subject(12,$subject_details);
+    //function call to get subject for grade 11
+    get_subject(11,$subject_details);
+    //function call to get subject for grade 10
+    get_subject(10,$subject_details);
     echo "</table><br>";
-    foreach ($details as $key=>$value) {
-        $gra=$value['class'];
-        $arr=$value['marks'];
+    foreach ($student_details as $key=>$value) {
+        $grade=$value['class'];
+        $marks=$value['marks'];
         $count=0;
-        foreach ($arr as $key1=>$val) {
+        foreach ($marks as $key1=>$val) {
             for($x=0;$x<4;$x++) {
-                if($subject[$gra][$x]['code']==$key1) {
-                    $details[$key]['mm'][$key1]=$subject[$gra][$x]['mm'];
-                    if($subject[$gra][$x]['mm']<=$arr[$key1]) {
+                if($subject_details[$grade][$x]['code']==$key1) {
+                    $student_details[$key]['mm'][$key1]=$subject_details[$grade][$x]['mm'];
+                    if($subject_details[$grade][$x]['mm']<=$marks[$key1]) {
                         $count=$count+1;
                     }
                 }
@@ -58,35 +61,35 @@
         }
     //
         if($count>=2) {
-            $details[$key]['res']="PASS";
+            $student_details[$key]['result']="PASS";
         }
         else {
-            $details[$key]['res']="FAIL";
+            $student_details[$key]['result']="FAIL";
         }
     }
-    //ANSWER --2 function return marks of student
-    function mark($marks) {
+    //function return marks of student
+    function get_mark($marks) {
         echo '<table border = "1px"><tr><th>Student id</th><th>Subject Code</th><th>Marks Obtained</th></tr>';
-            foreach ($marks as $mar) 
+            foreach ($marks as $mark) 
             {
-                echo "<tr><td>" . $mar['id'] . "</td><td>".$mar['code']."</td><td>".$mar['ob']."</td></tr>";        
+                echo "<tr><td>" . $mark['id'] . "</td><td>".$mark['code']."</td><td>".$mark['ob']."</td></tr>";        
             }
         echo '</table>';
     }
-    mark($marks);
+    get_mark($marks);
     echo "<br>";
-    // ANSWER --3
-    function student($details, $marks, $subject) {   
+    //final output
+    function student($student_details, $marks, $subject_details) {   
         echo '<table border = "1px"><th>NAME</th><th>DOB</th><th>GRADE</th><th>SUBJECTS</th><th>RESULT</th>';
-        foreach ($details as $key=>$value) {
+        foreach ($student_details as $key=>$value) {
             $arr=$value['marks'];
-            $details[$key]['dob']=date('m/d/Y', $details[$key]['dob']);
-            echo "<tr><td>" .$details[$key]['name']."</td><td>".$details[$key]['dob']."</td><td>".$details[$key]['class']."</td><td>";
+            $student_details[$key]['dob']=date('m/d/Y', $student_details[$key]['dob']);
+            echo "<tr><td>" .$student_details[$key]['name']."</td><td>".$student_details[$key]['dob']."</td><td>".$student_details[$key]['class']."</td><td>";
                 foreach ($arr as $key1=>$val) {
-                    echo $key1."(".$details[$key]['marks'][$key1].",".$details[$key]['mm'][$key1].")"."<br>";
+                    echo $key1."(".$student_details[$key]['marks'][$key1].",".$student_details[$key]['mm'][$key1].")"."<br>";
                 }
-            echo "</td><td>".$details[$key]['res']."</td></tr>";
+            echo "</td><td>".$student_details[$key]['res']."</td></tr>";
         }
     }
-    student ($details, $marks, $subject);
+    student ($student_details, $marks, $subject);
 ?>
